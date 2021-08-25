5ae10ee97e09d95422a18ba57915db71fe6a195a
Julien Maring <julienm@sipios.com>
2021-08-25 10:58:54 +0200

    feat(pokedex-routing): adding navigation guard for log in

--- pokedex-routing/src/app/app-routing.module.ts
+++ pokedex-routing/src/app/app-routing.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomePageComponent } from './home-page/home-page.component';
 import { PokedexPageComponent } from './pokedex-page/pokedex-page.component';
+import { IsLogInGuard } from 'src/app/is-log-in.guard';
 
 const routes: Routes = [
   { path: '', component: HomePageComponent },
-  { path: 'pokedex', component: PokedexPageComponent },
+  { path: 'pokedex', component: PokedexPageComponent, canActivate: [ IsLogInGuard] },
 ];
 
 @NgModule({
